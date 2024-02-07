58fb5c628cdcaf23cccd8b3b500c136a83a85cac
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-02-07 13:14:23 -0300

    Adicionado assets e estrutura inicial do projeto

---
+++ lib/src/binding/lab_clinicias_application_binding.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:lab_clinicas_adm/src/core/env.dart';
+import 'package:lab_clinicas_core/lab_clinicas_core.dart';
+
+class LabCliniciasApplicationBinding extends ApplicationBindings {
+  
+  @override
+  List<Bind<Object>> bindings() => [
+    Bind.lazySingleton<RestClient>((i) => RestClient(Env.backendBaseUrl)),
+  ];
+  
+}
